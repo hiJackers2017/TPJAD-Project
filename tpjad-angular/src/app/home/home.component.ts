3bfcd25d9247ff064d093f5079b4105488339b47
Claudia Pop <popclaudia97@gmail.com>
2020-02-02 14:19:06 +0200

    tpjad-angular: Home page

--- tpjad-angular/src/app/home/home.component.ts
+++ tpjad-angular/src/app/home/home.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
+import { User } from '../_models';
+import { UserService, AuthenticationService } from '../_services';
+
+@Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit {
+    currentUser: User;
+    users = [];
 
-  constructor() { }
+    constructor(
+        private authenticationService: AuthenticationService,
+        private userService: UserService
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.loadAllUsers();
+    }
 
+    deleteUser(id: number) {
+        this.userService.delete(id)
+            .pipe(first())
+            .subscribe(() => this.loadAllUsers());
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll()
+            .pipe(first())
+            .subscribe(users => this.users = users);
+    }
 }
